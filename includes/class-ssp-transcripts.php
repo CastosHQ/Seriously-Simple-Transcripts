bbdbb1dad850b5c4221c65671c2f0da1db9378f6
Sergey Zakharchenko <zaharchenko.dev@gmail.com>
2022-01-19 19:28:01 +0200

    Show notice if Enable Player meta data option is disabled #1

--- includes/class-ssp-transcripts.php
+++ includes/class-ssp-transcripts.php
@@ -1,14 +1,16 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 class SSP_Transcripts {
 
 	/**
 	 * The single instance of SSP_Transcripts.
-	 * @var 	object
+	 * @var    object
 	 * @access  private
-	 * @since 	1.0.0
+	 * @since    1.0.0
 	 */
 	private static $_instance = null;
 
@@ -73,18 +75,18 @@ class SSP_Transcripts {
 	 * @access  public
 	 * @since   1.0.0
 	 */
-	public function __construct ( $file = '', $version = '1.0.0' ) {
+	public function __construct( $file = '', $version = '1.0.0' ) {
 		global $wpdb;
 
 		// Load plugin constants
 		$this->_version = $version;
-		$this->_token = 'ssp_transcripts';
+		$this->_token   = 'ssp_transcripts';
 
 		// Load plugin environment variables
-		$this->file = $file;
-		$this->dir = dirname( $this->file );
-		$this->assets_dir = trailingslashit( $this->dir ) . 'assets';
-		$this->assets_url = esc_url( trailingslashit( plugins_url( '/assets/', $this->file ) ) );
+		$this->file          = $file;
+		$this->dir           = dirname( $this->file );
+		$this->assets_dir    = trailingslashit( $this->dir ) . 'assets';
+		$this->assets_url    = esc_url( trailingslashit( plugins_url( '/assets/', $this->file ) ) );
 		$this->script_suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 
 		register_activation_hook( $this->file, array( $this, 'install' ) );
@@ -102,34 +104,42 @@ class SSP_Transcripts {
 		add_action( 'plugins_loaded', array( $this, 'load_localisation' ) );
 	} // End __construct ()
 
-	public function add_field ( $fields = array() ) {
+	public function add_field( $fields = array() ) {
 
-		foreach( $fields as $key => $data ) {
+		foreach ( $fields as $key => $data ) {
 			$new_fields[ $key ] = $data;
 
-			if( 'audio_file' == $key ) {
+			if ( 'audio_file' == $key ) {
 				$new_fields['transcript_file'] = array(
-				    'name' => __( 'Transcript file:' , 'seriously-simple-transcripts' ),
-				    'description' => __( 'Upload the transcript file or paste the file URL here.' , 'seriously-simple-transcripts' ),
-				    'type' => 'file',
-				    'default' => '',
-				    'section' => 'info',
+					'name'        => __( 'Transcript file:', 'seriously-simple-transcripts' ),
+					'description' => __( 'Upload the transcript file or paste the file URL here.', 'seriously-simple-transcripts' ),
+					'type'        => 'file',
+					'default'     => '',
+					'section'     => 'info',
 				);
+
+				$meta_enabled = get_option( 'ss_podcasting_player_meta_data_enabled' );
+
+				if ( ! $meta_enabled ) {
+					$message =  __( 'To add a <strong>Download Transcript</strong> link, <a href="%s">Enable Player meta data</a>', 'seriously-simple-transcripts' );
+					$message = sprintf( $message, admin_url('edit.php?post_type=' . SSP_CPT_PODCAST . '&page=podcast_settings&tab=player-settings#player_meta_data_enabled') );
+					$new_fields['transcript_file']['description'] .= '<br><div class="warning">' . $message . '</div>';
+				}
 			}
 		}
 
 		return $new_fields;
 	}
 
-	public function display_link ( $meta = array(), $episode_id = 0, $context = '' ) {
+	public function display_link( $meta = array(), $episode_id = 0, $context = '' ) {
 
-		if( ! $episode_id ) {
+		if ( ! $episode_id ) {
 			return $meta;
 		}
 
 		$transcript_file = get_post_meta( $episode_id, 'transcript_file', true );
 
-		if( $transcript_file ) {
+		if ( $transcript_file ) {
 			$meta['transcript_file'] = sprintf( __( '%1$sDownload transcript%2$s', 'seriously-simple-transcripts' ), '<a href="' . esc_url( $transcript_file ) . '" target="_blank">', '</a>' );
 		}
 
@@ -139,10 +149,10 @@ class SSP_Transcripts {
 	/**
 	 * Load admin Javascript.
 	 * @access  public
-	 * @since   1.0.0
 	 * @return  void
+	 * @since   1.0.0
 	 */
-	public function admin_enqueue_scripts ( $hook = '' ) {
+	public function admin_enqueue_scripts( $hook = '' ) {
 		wp_register_script( $this->_token . '-admin', esc_url( $this->assets_url ) . 'js/admin' . $this->script_suffix . '.js', array( 'jquery' ), $this->_version );
 		wp_enqueue_script( $this->_token . '-admin' );
 	} // End admin_enqueue_scripts ()
@@ -150,10 +160,10 @@ class SSP_Transcripts {
 	/**
 	 * Load plugin localisation
 	 * @access  public
-	 * @since   1.0.0
 	 * @return  void
+	 * @since   1.0.0
 	 */
-	public function load_localisation () {
+	public function load_localisation() {
 		load_plugin_textdomain( 'seriously-simple-transcripts', false, basename( $this->dir ) . '/languages/' );
 	} // End load_localisation ()
 
@@ -162,15 +172,16 @@ class SSP_Transcripts {
 	 *
 	 * Ensures only one instance of SSP_Transcripts is loaded or can be loaded.
 	 *
+	 * @return Main SSP_Transcripts instance
+	 * @see SSP_Transcripts()
 	 * @since 1.0.0
 	 * @static
-	 * @see SSP_Transcripts()
-	 * @return Main SSP_Transcripts instance
 	 */
-	public static function instance ( $file = '', $version = '1.0.0', $db_version = '1.0.0' ) {
+	public static function instance( $file = '', $version = '1.0.0', $db_version = '1.0.0' ) {
 		if ( is_null( self::$_instance ) ) {
 			self::$_instance = new self( $file, $version, $db_version );
 		}
+
 		return self::$_instance;
 	} // End instance ()
 
@@ -179,7 +190,7 @@ class SSP_Transcripts {
 	 *
 	 * @since 1.0.0
 	 */
-	public function __clone () {
+	public function __clone() {
 		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?' ), $this->_version );
 	} // End __clone ()
 
@@ -188,27 +199,27 @@ class SSP_Transcripts {
 	 *
 	 * @since 1.0.0
 	 */
-	public function __wakeup () {
+	public function __wakeup() {
 		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?' ), $this->_version );
 	} // End __wakeup ()
 
 	/**
 	 * Installation. Runs on activation.
 	 * @access  public
-	 * @since   1.0.0
 	 * @return  void
+	 * @since   1.0.0
 	 */
-	public function install () {
+	public function install() {
 		$this->_log_version_number();
 	} // End install ()
 
 	/**
 	 * Log the plugin version number.
 	 * @access  public
-	 * @since   1.0.0
 	 * @return  void
+	 * @since   1.0.0
 	 */
-	private function _log_version_number () {
+	private function _log_version_number() {
 		update_option( $this->_token . '_version', $this->_version );
 	} // End _log_version_number ()
 
