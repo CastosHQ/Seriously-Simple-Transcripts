5904b042bff01b7e30d88c09b4a206914a0cec3d
Sergey Zakharchenko <zaharchenko.dev@gmail.com>
2022-12-02 17:38:41 +0200

    Version 1.1.1

--- seriously-simple-transcripts.php
+++ seriously-simple-transcripts.php
@@ -1,13 +1,13 @@
 <?php
 /*
  * Plugin Name: Seriously Simple Transcripts
- * Version: 1.1.1-beta
+ * Version: 1.1.1
  * Plugin URI: https://wordpress.org/plugins/seriously-simple-transcripts
  * Description: Add downloadable transcripts to your Seriously Simple Podcasting episodes.
  * Author: Castos
  * Author URI: https://www.castos.com/
  * Requires at least: 4.4
- * Tested up to: 5.0
+ * Tested up to: 6.1
  *
  * Text Domain: seriously-simple-transcripts
  *
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'SSP_TRANSCRIPTS_VERSION', '1.1.1-beta' );
+define( 'SSP_TRANSCRIPTS_VERSION', '1.1.1' );
 define( 'SSP_TRANSCRIPTS_PLUGIN_FILE', __FILE__ );
 define( 'SSP_TRANSCRIPTS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'SSP_TRANSCRIPTS_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
