f542c97c32fd22d1fee2909af2de8e342d3c3fcc
Serhiy Zakharchenko <zaharchenko.dev@gmail.com>
2024-11-22 14:57:52 +0000

    Sidebar file settings

--- php/classes/controllers/class-assets-controller.php
+++ php/classes/controllers/class-assets-controller.php
@@ -20,5 +20,20 @@ class Assets_Controller extends Abstract_Controller {
 	public function admin_enqueue_scripts() {
 		wp_register_script( $this->_token . '-admin', esc_url( $this->assets_url ) . 'js/admin' . $this->script_suffix . '.js', array( 'jquery' ), $this->_version );
 		wp_enqueue_script( $this->_token . '-admin' );
+
+		$ssp_post_types = ssp_post_types();
+		$screen = get_current_screen();
+
+		// Make sure sidebar script loads only on SSP posts.
+		if ( 'post' != $screen->base || ! $screen->post_type ||
+		     ! in_array( $screen->post_type, $ssp_post_types ) ) {
+			return;
+		}
+
+		wp_enqueue_script( $this->_token . '-sidebar', SSP_TRANSCRIPTS_PLUGIN_URL . 'build/plugins/sidebar/index.js',
+			array( 'wp-plugins', 'wp-edit-post', 'wp-element', 'wp-components', 'wp-data' ),
+			$this->_version,
+			true
+		);
 	}
 }
